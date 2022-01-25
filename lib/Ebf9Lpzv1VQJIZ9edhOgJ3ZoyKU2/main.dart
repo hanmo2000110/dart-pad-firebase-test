530eb8e19aa84479b766f3993c581ed9f096c2d0
hanmo2000110 <74346222+hanmo2000110@users.noreply.github.com>
2022-01-25 16:07:46 +0900

    hosting test

---
+++ lib/Ebf9Lpzv1VQJIZ9edhOgJ3ZoyKU2/main.dart
@@ -0,0 +1,34 @@
+String stringify(int x, int y) {
+  return '$x $y';
+}
+
+void main() {
+  try {
+    final str = stringify(2, 3); 
+
+    if (str == '2 3') {
+      _result(true);
+    } else if (str == '23') {
+      _result(false, ['Test failed. It looks like you forgot the space!']);
+    } else if (str == null) {
+      _result(false, ['Test failed. Did you forget to return a value?']);
+    } else {
+      _result(false, ['That\'s not quite right. Keep trying!']);
+    }
+  } catch (e) {
+    _result(false, ['Tried calling stringify(2, 3), but received an exception: ${e.runtimeType}']);
+  }
+}
+
+void _result(bool success, [List<String> messages = const []]) {
+  // Join messages into a comma-separated list for inclusion in the JSON array.
+  final joinedMessages = messages.map((m) => '"$m"').join(',');
+  print('__TESTRESULT__ {"success": $success, "messages": [$joinedMessages]}');
+}
+
+// Ensure we have at least one use of `_result`.
+var resultFunction = _result;
+
+// Placeholder for unimplemented methods in dart-pad exercises.
+// ignore: non_constant_identifier_names, sdk_version_never
+Never TODO([String message = '']) => throw UnimplementedError(message);
